384691f329dda907962aaee8860e3e1b0afea3c7
Nabil <nlamriben@vcinc.com>
2024-07-14 19:57:49 -0400

    newdiag

---
+++ FormSerialTester.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace LiteCanSimProj
+{
+    partial class FormSerialTester
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.trackBarSlider = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(125, 288);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(188, 25);
+            this.lblStatus.TabIndex = 47;
+            this.lblStatus.Text = "Messagestype103";
+            // 
+            // comboBoxPorts
+            // 
+            this.comboBoxPorts.FormattingEnabled = true;
+            this.comboBoxPorts.Location = new System.Drawing.Point(223, 138);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new System.Drawing.Size(163, 33);
+            this.comboBoxPorts.TabIndex = 45;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(416, 138);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(259, 79);
+            this.btnStart.TabIndex = 46;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // trackBarSlider
+            // 
+            this.trackBarSlider.Location = new System.Drawing.Point(116, 12);
+            this.trackBarSlider.Name = "trackBarSlider";
+            this.trackBarSlider.Size = new System.Drawing.Size(104, 90);
+            this.trackBarSlider.TabIndex = 48;
+            // 
+            // FormSerialTester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBarSlider);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.comboBoxPorts);
+            this.Controls.Add(this.btnStart);
+            this.Name = "FormSerialTester";
+            this.Text = "FormSerialTester";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSlider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TrackBar trackBarSlider;
+    }
+}
---
+++ FormSerialTester.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LiteCanSimProj
+{
+    public partial class FormSerialTester : Form
+    {
+        private const int BaudRate = 19200;
+        private SerialPort serialPort;
+        private CancellationTokenSource cancellationTokenSource;
+        public FormSerialTester()
+        {
+            InitializeComponent();
+            comboBoxPorts.DropDown += new EventHandler(Serial_DropDown);
+            PopulateSerialPorts(comboBoxPorts);
+            btnStart.Click += btnStart_Click;
+        }
+        private void PopulateSerialPorts(ComboBox comboBox)
+        {
+            try
+            {
+                object selectedItem = comboBox.SelectedItem;
+                comboBox.Items.Clear();
+                string[] portNames = SerialPort.GetPortNames();
+                if (portNames.Length == 0)
+                {
+                    MessageBox.Show("No serial ports found on this system.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                comboBox.Items.AddRange(portNames);
+                comboBox.SelectedItem = selectedItem;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error fetching serial ports: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Serial_DropDown(object sender, EventArgs e)
+        {
+            if (sender == null)
+                return;
+            ComboBox comboBox = (ComboBox)sender;
+            object selectedItem = comboBox.SelectedItem;
+            comboBox.Items.Clear();
+            comboBox.Items.AddRange(SerialPort.GetPortNames());
+            comboBox.SelectedItem = selectedItem;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                StopSendingData();
+            }
+            else if (comboBoxPorts.SelectedItem == null)
+            {
+                MessageBox.Show("Select a serial port to start.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                StartSendingData();
+            }
+        }
+
+        private void StartSendingData()
+        {
+            serialPort = new SerialPort((string)comboBoxPorts.SelectedItem, BaudRate);
+
+            try
+            {
+                serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening port: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            btnStart.Text = "Stop";
+            comboBoxPorts.Enabled = false;
+
+            cancellationTokenSource = new CancellationTokenSource();
+            Task.Run(() => SendDataAsync(cancellationTokenSource.Token));
+        }
+
+        private void StopSendingData()
+        {
+            cancellationTokenSource.Cancel();
+            serialPort.Close();
+            btnStart.Text = "Start";
+            comboBoxPorts.Enabled = true;
+        }
+
+        private async Task SendDataAsync(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                string message = $"<1,{trackBarSlider.Value},600,800,0,1,0,0>";
+                try
+                {
+                    serialPort.Write(message);
+                    Invoke(new Action(() =>
+                    {
+                        lblStatus.Text = $"Sent: {message}";
+                    }));
+                }
+                catch (Exception ex)
+                {
+                    Invoke(new Action(() =>
+                    {
+                        MessageBox.Show("Error writing to port: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        StopSendingData();
+                    }));
+                    break;
+                }
+
+                await Task.Delay(250, cancellationToken);
+            }
+        }
+    }
+}
